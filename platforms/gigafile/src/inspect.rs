bc3cc660239003d3f89aac5ec0514fcb95d712f7
Yesterday17 <mmf@mmf.moe>
2025-10-05 00:50:55 +0900

    refactor(gigafile): inspector

--- platforms/gigafile/src/inspect.rs
+++ platforms/gigafile/src/inspect.rs
@@ -1,5 +1,4 @@
 use fake_user_agent::get_chrome_rua;
-use regex::bytes::Regex;
 use reqwest::{
     Client,
     header::{CONTENT_DISPOSITION, COOKIE, USER_AGENT},
@@ -8,101 +7,83 @@ use shiori_plugin::*;
 
 use crate::client::GigafileClient;
 
-pub struct GigafileInspector;
+pub struct GigafilePlugin;
 
-impl InspectorBuilder for GigafileInspector {
-    fn name(&self) -> String {
-        "gigafile".to_string()
+impl ShioriPlugin for GigafilePlugin {
+    fn name(&self) -> impl Future<Output = String> {
+        async { "gigafile".to_string() }
     }
 
-    fn help(&self) -> Vec<String> {
-        [
-            "Extracts raw download URL from Gigafile.",
-            "",
-            "Template:",
-            "- https://*.gigafile.nu/*",
-        ]
-        .iter()
-        .map(|s| s.to_string())
-        .collect()
+    fn version(&self) -> impl Future<Output = String> {
+        async { "0.1.0".to_string() }
     }
 
-    fn arguments(&self, command: &mut dyn shiori_plugin::InspectorCommand) {
-        command.add_argument("giga-key", Some("key"), "[Gigafile] Download key");
+    fn description(&self) -> impl Future<Output = Option<String>> {
+        async { Some("Extracts raw download URL from Gigafile.".to_string()) }
     }
 
-    fn build(
+    async fn register(
         &self,
-        args: &dyn shiori_plugin::InspectorArguments,
-    ) -> anyhow::Result<Box<dyn shiori_plugin::Inspect>> {
-        Ok(Box::new(GigafileInspectorImpl(args.get_string("giga-key"))))
+        mut registry: impl Registry,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        registry.add_argument("giga-key", Some("key"), "[Gigafile] Download key");
+
+        let regex = Regex::new(r"https://(\d+)\.gigafile\.nu/.*").unwrap();
+        registry.register_inspector(regex, Box::new(GigafileInspector), PriorityHint::Normal);
+        Ok(())
     }
 }
 
-struct GigafileInspectorImpl(Option<String>);
+struct GigafileInspector;
 
 #[async_trait]
-impl Inspect for GigafileInspectorImpl {
-    async fn register(
+impl Inspect for GigafileInspector {
+    async fn inspect(
         &self,
-        id: InspectorIdentifier,
-        registry: &mut InspectRegistry,
-    ) -> anyhow::Result<()> {
-        let key = self.0.clone();
+        url: &str,
+        _captures: &regex::Captures,
+        args: &dyn InspectorArguments,
+    ) -> anyhow::Result<InspectResult> {
+        let key = args.get_string("giga-key");
+        let client = GigafileClient::new(key);
+        let (url, cookie) = client.get_download_url(url.try_into()?).await?;
 
-        registry.register_http_route(
-            RouterScheme::Https,
-            "*.gigafile.nu".as_bytes(),
-            "/{file_id}",
-            (
-                id,
-                Box::new(move |url, _| {
-                    let client = GigafileClient::new(key.clone());
-                    Box::pin(async move {
-                        let (url, cookie) = client.get_download_url(url).await?;
+        let client = Client::builder()
+            .danger_accept_invalid_certs(true)
+            .build()
+            .unwrap();
+        let response = client
+            .get(&url)
+            .header(COOKIE, &cookie)
+            .header(USER_AGENT, get_chrome_rua())
+            .send()
+            .await?;
+        let filename = response.headers().get(CONTENT_DISPOSITION).and_then(|v| {
+            // attachment; filename="<filename>";
+            let re = regex::bytes::Regex::new(r#"filename="([^"]+)""#).unwrap();
+            let matched = re
+                .captures(v.as_bytes())
+                .and_then(|c| c.get(1).map(|m| m.as_bytes()))?;
+            let filename = String::from_utf8(matched.to_vec()).ok()?;
+            Some(filename)
+        });
+        drop(response);
 
-                        let client = Client::builder()
-                            .danger_accept_invalid_certs(true)
-                            .build()
-                            .unwrap();
-                        let response = client
-                            .get(&url)
-                            .header(COOKIE, &cookie)
-                            .header(USER_AGENT, get_chrome_rua())
-                            .send()
-                            .await?;
-                        let filename = response.headers().get(CONTENT_DISPOSITION).and_then(|v| {
-                            // attachment; filename="<filename>";
-                            let re = Regex::new(r#"filename="([^"]+)"#).unwrap();
-                            let matched = re
-                                .captures(v.as_bytes())
-                                .and_then(|c| c.get(1).map(|m| m.as_bytes()))?;
-                            let filename = String::from_utf8(matched.to_vec()).ok()?;
-                            Some(filename)
-                        });
-                        drop(response);
+        let filename = filename.map(|f| {
+            let (name, ext) = f.rsplit_once('.').unwrap_or((&f, "raw"));
+            (name.to_string(), ext.to_string())
+        });
+        let (title, ext) = match filename {
+            Some((filename, ext)) => (Some(filename), ext),
+            None => (None, "raw".to_string()),
+        };
 
-                        let filename = filename.map(|f| {
-                            let (name, ext) = f.rsplit_once('.').unwrap_or((&f, "raw"));
-                            (name.to_string(), ext.to_string())
-                        });
-                        let (title, ext) = match filename {
-                            Some((filename, ext)) => (Some(filename), ext),
-                            None => (None, "raw".to_string()),
-                        };
-
-                        Ok(InspectResult::Playlist(InspectPlaylist {
-                            title,
-                            playlist_url: url,
-                            playlist_type: PlaylistType::Raw(ext),
-                            headers: vec![format!("Cookie: {cookie}")],
-                            ..Default::default()
-                        }))
-                    })
-                }),
-            ),
-        )?;
-
-        Ok(())
+        Ok(InspectResult::Playlist(InspectPlaylist {
+            title,
+            playlist_url: url,
+            playlist_type: PlaylistType::Raw(ext),
+            headers: vec![format!("Cookie: {cookie}")],
+            ..Default::default()
+        }))
     }
 }
